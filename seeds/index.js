779af5a35d69e20f7bf3f104078b923615ba8ee5
Anurag <anurag1822003@gmail.com>
2025-07-06 22:02:13 +0530

    Add error.ejs and update .gitignore

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://picsum.photos/400?random=${Math.random()}',
+            image: `https://picsum.photos/400?random=${Math.random()}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
