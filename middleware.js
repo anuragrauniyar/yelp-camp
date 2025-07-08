5ee134487506e0d26175d1dcc227a35069393b46
Anurag <anurag1822003@gmail.com>
2025-07-08 21:12:37 +0530

    updated login

--- middleware.js
+++ middleware.js
@@ -1,3 +1,10 @@
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
