811ac379ef492fdf27ab1862b5a963a200140d96
Anurag <anurag1822003@gmail.com>
2025-07-09 10:38:52 +0530

    implemented controllers and improved the stars reviee model

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '686d0d7b10c386153ca2aa43',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
